63d16cf41fa83fdcba7f42109b031aab954cfa85
yakushimizu <97076369+yakushimizu@users.noreply.github.com>
2022-01-30 05:14:54 +0000

    dmm

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   end
   resources :users, only: [:index,:show,:edit,:update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
 
 
