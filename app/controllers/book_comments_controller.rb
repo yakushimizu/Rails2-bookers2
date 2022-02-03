af6b44f4d68e53fa7e90dfcd7fb3355c957ebd08
yakushimizu <97076369+yakushimizu@users.noreply.github.com>
2022-02-03 08:14:53 +0000

    ss

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,17 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    comment = BookComment.new(book_comment_params)
-    comment.user_id = current_user.id
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    render :comments
   end
 
   def destroy
    BookComment.find(params[:id]).destroy
-   redirect_to book_path(params[:book_id])
+   @book = Book.find(params[:book_id])
+   render :comments
   end
 
   private
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,8 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
+
 
 Rails.start()
 Turbolinks.start()
